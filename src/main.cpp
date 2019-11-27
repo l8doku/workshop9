697a3e0cc159bc1204961e97040cf0f5b3612cc7
Georgii Zhulikov <l8doku@gmail.com>
2019-11-27 14:13:13 +0300

    Add file reading function

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
  *
  * 1) Download the dataset (https://simplemaps.com/data/world-cities), unzip and use it (.csv) in current workshop.
  *
- * 2) Create a vector<vector<string>> worldCities (vector of lines) where you will store the data.
- * Use worldCities for next tasks.
+ * 2) Create a vector<vector<string>> worldCities (vector of lines) 
+ * where you will store the data. Use worldCities for next tasks.
  *
- * 3) Create a function called howManyCountries. You should count countries and print them out.
+ * 3) Create a function called howManyCountries. 
+ * You should count countries and print them out.
  *
  * 4) Create a function which prints countries and their capitals.
  *
@@ -39,10 +40,50 @@
  * 7.2) return distance (distance between cities (two paired coordinates -- distance(pair(lat_1,lng_1),pair(lat_2,lng_2))
  * 7.3) to be continued...
  ******************************************************************************/
-
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
-int main()
+
+
+
+std::vector< std::vector<std::string> > fillWorldCities(std::istream& inputFile)
 {
-    // put our code here
+    std::vector<std::vector<std::string>> worldCities;
+    
+    std::string line;
+    std::vector <std::string> lineVector;
+    while (!inputFile.eof() && inputFile.good())
+    {
+        lineVector.clear();
+        std::getline (inputFile, line);
+        std::stringstream ss(line);
+        std::string token;
+
+        while (!ss.eof() && ss.good())
+        {
+            std::getline(ss, token,',');
+            lineVector.push_back(token);
+        }
+        worldCities.push_back(lineVector);
+    }
+
+    return worldCities;
+}
+
+
+
+int main ()
+{
+    
+    std::vector<std::vector<std::string>> worldCities;
+    
+    const std::string INP_FILE_NAME = "/home/georgii/WORK/DSBA/repositories/workshop8/workshop8/tasks/input.csv";
+    std::ifstream inputFile;
+    inputFile.open(INP_FILE_NAME);
+    
+    
+    
+
 }
